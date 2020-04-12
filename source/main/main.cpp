7d6a981829abc31c3adf13a28057224bbb208080
Dmitriy Koltun <d_koltun@wargaming.net>
2020-04-12 18:55:19 +0300

    added hello world of opengl main.cpp

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -1,13 +1,60 @@
-#include "../cooling/test_dll.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+// Включаем GLEW. Всегда включайте его ДО gl.h и glfw.h
+#include <GL/glew.h>
+// Включаем GLFW
+#include <GLFW/glfw3.h>
 
-#include <iostream>
+// Включаем GLM
+#include <glm/glm.hpp>
+using namespace glm;
 
-using namespace std;
+int main() {
 
-int main()
-{
-	Test ddl;
-	ddl.foo();
-	ddl.foo1();
-     return 0;
+	// Инициализируем GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Ошибка при инициализации GLFWn");
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x Сглаживание
+	glfwWindowHint(GLFW_VERSION_MAJOR, 3); // Мы хотим использовать OpenGL 3.3
+	glfwWindowHint(GLFW_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Мы не хотим старый OpenGL
+
+	// Открыть окно и создать в нем контекст OpenGL
+	GLFWwindow* window; // (В сопроводительном исходном коде эта переменная является глобальной)
+	window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Невозможно открыть окно GLFW. Если у вас Intel GPU, то он не поддерживает версию 3.3. Попробуйте версию уроков для OpenGL 2.1.n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Инициализируем GLEW
+	glewExperimental = true; // Флаг необходим в Core-режиме OpenGL
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Невозможно инициализировать GLEWn");
+		return -1;
+	}
+
+	// Включим режим отслеживания нажатия клавиш, для проверки ниже
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		// Пока что ничего не выводим. Это будет в уроке 2.
+
+		// Сбрасываем буферы
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Проверяем нажатие клавиши Escape или закрытие окна
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
+	system("pause");
+	return 0;
 }
